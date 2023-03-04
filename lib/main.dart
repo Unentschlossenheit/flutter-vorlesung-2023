7451d54b6d31155fce2b99797aa236bea3351da1
Cirrus CI <support@cirruslabs.org>
2023-03-04 10:02:42 +0000

    added decrement button from task 1

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -90,19 +96,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -112,8 +105,22 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+
+            Row(
+          children: [
+            ElevatedButton(
+            style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+            onPressed: _decrementCounter,
+            child: const Text('Decrement Counter'),
+            ),
+          ],
+          )
+          
           ],
         ),
+        
+        
+          
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
